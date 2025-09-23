216be549a9c42e561d2d4cf589e7d9aba988b4fa
robgriffin247 <robgriffin247@hotmail.com>
2025-09-23 11:25:39 +0200

    added atgni

--- dlt/zwiftracing.py
+++ dlt/zwiftracing.py
@@ -94,8 +94,9 @@ def load(resource, id):
 
 
 if __name__ == "__main__":
-    print(load("rider", 4598636))
+    #print(load("rider", 4598636))
     #print(load("rider", 5574))
     #print(load("riders", [4598636, 5574, 5879996, 7252254, 5639087, 5913482, 685362, 6120611, 1240469]))
     #[12764, 161, 20650, 2707, 2740]
     #print(load("club", 2740))
+    print(load("club", 20138))
