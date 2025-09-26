1bfb339adaaa0af43008e0234755ea97782e47bb
robgriffin247 <robgriffin247@hotmail.com>
2025-09-26 16:00:51 +0200

    added dbt-utils for tests

--- dbt_zs/macros/max_value.sql
+++
@@ -1,5 +0,0 @@
-{% test max_value(model, column_name, threshold) %}
-
-select {{ column_name }} from {{ model }} where {{ column_name }} > {{ threshold }}
-
-{% endtest %}
--- dbt_zs/tests/no_riders_over_100kg_test.sql
+++
@@ -1,3 +0,0 @@
-{{ config(severity = 'warn') }}
-
-select id from {{ ref("obt__riders") }} where weight > 100 group by 1
